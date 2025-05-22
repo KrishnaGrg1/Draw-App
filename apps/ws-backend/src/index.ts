d0a418a3eec0bd8d463413683dbd26520c1aa122
KrishnaGrg1 <gkrishnabahadur618@gmail.com>
2025-05-23 00:27:52 +0545

    adding web-socket

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,33 @@
-import { WebSocketServer } from "ws";
+import WebSocket, { WebSocketServer } from "ws";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 
 const wss = new WebSocketServer({ port: 8080 });
+
+interface User {
+  userId: string;
+  ws: WebSocket;
+  rooms: string[];
+}
+const users: User[] = [];
+
+function checkUser(token: string): string | null {
+  try{
+
+    const decode = jwt.verify(token, JWT_SECRET) as JwtPayload;
+    if (typeof decode === "string") {
+      return null;
+    }
+    
+    if (!decode || !decode.userId) {
+      return null;
+    }
+    return decode.userId;
+  }catch(e){
+    return null
+  }
+  return null
+}
 
 wss.on("connection", (ws, request) => {
   try {
@@ -19,25 +44,57 @@ wss.on("connection", (ws, request) => {
       ws.close(1008, "Missing token");
       return;
     }
-    
 
-    const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+    const userId = checkUser(token);
 
-    if (!decoded || !decoded.userId) {
-      ws.close(1008, "Invalid token");
-      return;
+    if (!userId) {
+      ws.close();
+      return null;
     }
-
-    // At this point, user is authenticated
-    const userId = decoded.userId;
+    users.push({
+      userId,
+      rooms: [],
+      ws: ws
+    });
 
     console.log(`User connected: ${userId}`);
 
     ws.on("message", (data) => {
-      // Echo or handle message
-      ws.send("pong");
-    });
+      const parsedData = JSON.parse(data as unknown as string);
 
+      if (parsedData.type === "join_room") {
+        const user = users.find((x) => x.ws === ws);
+        user?.rooms.push(parsedData.roomId);
+      }
+
+      if (parsedData.type === "leave_room") {
+        const user = users.find((x) => x.ws === ws);
+        if (!user) {
+          return;
+        }
+        user.rooms = user?.rooms.filter((x) => x === parsedData.room);
+      }
+
+      if(parsedData.type==='chat'){
+        const user=users.find(x=>x.ws===ws)
+        if(user){
+          return
+        }
+        const roomId=parsedData.roomId;
+        const message=parsedData.message;
+ 
+       users.forEach(user=> {
+        if(user.rooms.includes(roomId)){
+          user.ws.send(JSON.stringify({
+            type:"chat",
+            message,
+            roomId,
+            from:user.userId
+          }))
+        }
+       }); 
+      }
+    });
   } catch (err) {
     console.error("WebSocket auth error:", err);
     ws.close(1008, "Authentication failed");
